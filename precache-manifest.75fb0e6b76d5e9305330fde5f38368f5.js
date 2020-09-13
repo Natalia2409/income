aae2a9758cfbbae2baa1934b153dd52a7728b0d5
Natalia <nataliarudomyr1@gmail.com>
2020-09-13 20:08:49 +0300

    Updates

---
+++ precache-manifest.75fb0e6b76d5e9305330fde5f38368f5.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44844b5431a0c631644f53193922f14b",
+    "url": "/income/index.html"
+  },
+  {
+    "revision": "22ad20abdc342145f4ca",
+    "url": "/income/static/css/main.fe8bc7f8.chunk.css"
+  },
+  {
+    "revision": "b993069c8d72a315fc34",
+    "url": "/income/static/js/2.c183b5e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/income/static/js/2.c183b5e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ad20abdc342145f4ca",
+    "url": "/income/static/js/main.81f649bf.chunk.js"
+  },
+  {
+    "revision": "3a9a1f20b1ec2219e576",
+    "url": "/income/static/js/runtime-main.9648a687.js"
+  },
+  {
+    "revision": "990af8d7344ac31730f8b65c69f2c3f1",
+    "url": "/income/static/media/beer.990af8d7.png"
+  },
+  {
+    "revision": "fc2ba40b15e9cec61b5db9eb62899f6f",
+    "url": "/income/static/media/bus.fc2ba40b.png"
+  },
+  {
+    "revision": "49b4572c4a23befc3520534b76b37801",
+    "url": "/income/static/media/cafe.49b4572c.png"
+  },
+  {
+    "revision": "c5d99b5cee9fbc9b3c99e02209652b3b",
+    "url": "/income/static/media/car.c5d99b5c.png"
+  },
+  {
+    "revision": "c54a6e9006069e755054537598215067",
+    "url": "/income/static/media/clothes.c54a6e90.png"
+  },
+  {
+    "revision": "67f64e54ad4e864398d3779a145a5808",
+    "url": "/income/static/media/dog.67f64e54.png"
+  },
+  {
+    "revision": "7cbf6d7704484301533a703be36356fd",
+    "url": "/income/static/media/food.7cbf6d77.png"
+  },
+  {
+    "revision": "efdf1fa41fb2ccf8cd198730827d9aa1",
+    "url": "/income/static/media/gift.efdf1fa4.png"
+  },
+  {
+    "revision": "8512b59a3af7950e54fa445e4f41a3a1",
+    "url": "/income/static/media/medicine.8512b59a.png"
+  },
+  {
+    "revision": "273a3a3b30a9fa655eba5a1d7d6a2f75",
+    "url": "/income/static/media/muscle.273a3a3b.png"
+  },
+  {
+    "revision": "5a89f6308254ec6ecd1c41989c6acfc1",
+    "url": "/income/static/media/property.5a89f630.png"
+  },
+  {
+    "revision": "9048e39e9f256bd8e3e41c0e70b7918f",
+    "url": "/income/static/media/taxi.9048e39e.png"
+  },
+  {
+    "revision": "4fa1f29193d723670a282abf7fadd46d",
+    "url": "/income/static/media/toothbrush.4fa1f291.png"
+  }
+]);
